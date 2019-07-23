20db8ac61a2c4ee575fb6934fa1f779566719e12
Juan Palacios <jpalaciosdev@gmail.com>
2019-07-23 19:26:46 +0200

    Fix typo on format.sh

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Formating code..."
+echo "Formatting code..."
 
 find . -type d -name 3rdparty -prune -o -type f \( -name "*\.h" -o -name "*\.cpp" \) -print0 | xargs -0 -I '{}' sh -c "clang-format -i {}; echo -n '.'"
 
